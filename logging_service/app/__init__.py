4c15b6c166a0a810f041141bfabc4a5b7b6609a8
Danila Smyshliaev <den2003k12@gmail.com>
2025-03-30 19:56:54 +0200

    feat: add kafka + set up docker-compose config

--- logging_service/app/__init__.py
+++ logging_service/app/__init__.py
@@ -1,7 +1,13 @@
 # logging_service/app/__init__.py
-
+from flask import Flask
 from .models import db
-db.init_app(app)
-
-# Import models
 from .models.log_entry import LogEntry
+from .blueprints.api import api_bp
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object('app.config.Config')
+    db.init_app(app)
+    app.register_blueprint(api_bp)
+    return app
---
+++ logging_service/app/log_ingester.py
@@ -0,0 +1,37 @@
+import os
+from confluent_kafka import Producer
+import json
+
+# Ensure the KAFKA_BROKER environment variable is set
+kafka_broker = os.getenv('KAFKA_BROKER')
+if not kafka_broker:
+    raise EnvironmentError("KAFKA_BROKER environment variable is not set")
+
+# Kafka configuration
+conf = {
+    'bootstrap.servers': 'kafka:9092',  # Use the KAFKA_BROKER environment variable
+}
+
+# Create a Kafka producer instance
+producer = Producer(conf)
+
+def delivery_report(err, msg):
+    """ Called once for each message produced to indicate delivery result.
+        Triggered by poll() or flush(). """
+    if err is not None:
+        print(f'Delivery failed for message: {err}')
+    else:
+        print(f'Message delivered to {msg.topic()} [{msg.partition()}]')
+
+
+def produce_log(log_data):
+    """ Function to produce logs to the 'logs' topic """
+    try:
+        print(log_data)
+        # Produce the log message to the 'logs' topic
+        producer.produce('logs', value=json.dumps(log_data), callback=delivery_report)
+        # Wait up to 1 second for events. Callbacks will be invoked during
+        # this method call if the message is acknowledged.
+        producer.poll(1)
+    except Exception as e:
+        print(f'Failed to produce log: {e}')
