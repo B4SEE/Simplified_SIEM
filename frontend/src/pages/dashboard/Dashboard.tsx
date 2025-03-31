1b1b5d88cbdceb6040dbb8bbcb33b5feb438832d
vhs-cake <vali.kravtsova@gmail.com>
2025-03-31 21:12:58 +0200

    dashboard added

--- frontend/src/pages/dashboard/Dashboard.tsx
+++ frontend/src/pages/dashboard/Dashboard.tsx
@@ -1,5 +1,49 @@
-const Dashboard = () => {
-  return <h1>Dashboard Statistics</h1>;
+import React from 'react';
+import { Typography } from '@mui/material';
+import { mockStatsData } from '../../mock-data/mockStatsData';
+import {
+  DashboardContainer,
+  Title,
+  StatBoxContainer,
+  StatBox,
+  LastLoginText,
+} from './StyledDashboard';
+import colors from '../../theme/colors';
+
+const Dashboard: React.FC = () => {
+  return (
+    <DashboardContainer>
+      <Title variant='h4' gutterBottom>
+        Dashboard
+      </Title>
+
+      <StatBoxContainer>
+        <StatBox color={colors.totalLogs}>
+          <Typography variant='h6'>Total Logs</Typography>
+          <Typography variant='h4'>{mockStatsData.totalLogs}</Typography>
+        </StatBox>
+
+        <StatBox color={colors.successfulLogins}>
+          <Typography variant='h6'>Successful Logins</Typography>
+          <Typography variant='h4'>{mockStatsData.successfulLogins}</Typography>
+        </StatBox>
+
+        <StatBox color={colors.failedLogins}>
+          <Typography variant='h6'>Failed Logins</Typography>
+          <Typography variant='h4'>{mockStatsData.failedLogins}</Typography>
+        </StatBox>
+
+        <StatBox color={colors.activeAlerts}>
+          <Typography variant='h6'>Active Alerts</Typography>
+          <Typography variant='h4'>{mockStatsData.alertsCount}</Typography>
+        </StatBox>
+      </StatBoxContainer>
+
+      <LastLoginText variant='h6'>
+        Last Login: {mockStatsData.lastLogin}
+      </LastLoginText>
+    </DashboardContainer>
+  );
 };
 
 export default Dashboard;
