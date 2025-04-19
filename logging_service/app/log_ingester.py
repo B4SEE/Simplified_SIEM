1c66aecffd5e07bb264b44fcfbdebb762d30a2e8
M2kura <136367855+M2kura@users.noreply.github.com>
2025-04-19 19:11:27 +0200

    Squashed merge from feature/docker-configuration

--- logging_service/app/log_ingester.py
+++ logging_service/app/log_ingester.py
@@ -1,45 +1,68 @@
 import os
 import sys
-from time import sleep
-
-from confluent_kafka import Producer
-from kafka.admin import KafkaAdminClient, NewTopic
 import json
+from confluent_kafka import Producer
+from confluent_kafka.admin import AdminClient, NewTopic
+
+class KafkaProducerSingleton:
+    __instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if not cls.__instance:
+            cls.__instance = super(KafkaProducerSingleton, cls).__new__(cls)
+            try:
+                cls.__instance.__initialize()
+            except Exception as e:
+                print(f"Error initializing Kafka producer: {e}", file=sys.stderr)
+                cls.__instance = None
+        return cls.__instance
+
+    def __initialize(self):
+        # Ensure the KAFKA_BROKER environment variable is set
+        kafka_broker = os.getenv('KAFKA_BROKER', 'kafka:9092')
+        if not kafka_broker:
+            raise EnvironmentError("KAFKA_BROKER environment variable is not set")
+
+        # Kafka configuration
+        conf = {'bootstrap.servers': kafka_broker}
+
+        # Create an admin client
+        admin_client = AdminClient({'bootstrap.servers': kafka_broker})
+
+        # Define the topic
+        topic_name = 'logs'
+        num_partitions = 1
+        replication_factor = 1
+
+        # Check if the topic already exists
+        existing_topics = admin_client.list_topics().topics
+        print(f'Existing topics: {existing_topics}', file=sys.stderr)
+        if topic_name not in existing_topics:
+            # Create the topic
+            topic_list = [NewTopic(topic=topic_name, num_partitions=num_partitions, replication_factor=replication_factor)]
+            admin_client.create_topics(new_topics=topic_list, validate_only=False)
+            print(f'Topic "{topic_name}" was created', file=sys.stderr)
+        else:
+            print(f'Topic "{topic_name}" already exists, skip', file=sys.stderr)
+
+        # Create a Kafka producer instance
+        self.__producer = Producer(conf)
+        print("Kafka producer initialized", file=sys.stderr)
+
+    def __produce(self, topic, value, callback):
+        self.__producer.produce(topic, value=value, callback=callback)
+        self.__producer.flush()
 
 
-# Ensure the KAFKA_BROKER environment variable is set
-kafka_broker = os.getenv('KAFKA_BROKER', 'kafka:9092')
-if not kafka_broker:
-    raise EnvironmentError("KAFKA_BROKER environment variable is not set")
-
-# Kafka configuration
-conf = {
-    'bootstrap.servers': 'kafka:9092',  # Use the KAFKA_BROKER environment variable
-}
-
-# Create an admin client
-admin_client = KafkaAdminClient(
-    bootstrap_servers='kafka:9092',
-    client_id='log_topic_creator'
-)
-
-# Define the topic
-topic_name = 'logs'
-num_partitions = 1
-replication_factor = 1
-
-# Check if the topic already exists
-existing_topics = admin_client.list_topics()
-if topic_name not in existing_topics:
-    # Create the topic
-    topic_list = [NewTopic(name=topic_name, num_partitions=num_partitions, replication_factor=replication_factor)]
-    admin_client.create_topics(new_topics=topic_list, validate_only=False)
-else:
-    print(f'Topic "{topic_name}" already exists, skip', file=sys.stderr)
-
-
-# Create a Kafka producer instance
-producer = Producer(conf)
+    def produce_log(self, log_data):
+        """ Function to produce logs to the 'logs' topic """
+        try:
+            self.__produce('logs', value=json.dumps(log_data), callback=delivery_report)
+            print('produce data: ' + json.dumps(log_data), file=sys.stderr)
+            return True
+        except Exception as e:
+            print(f'Failed to produce log: {e}', file=sys.stderr)
+            return False
 
 def delivery_report(err, msg):
     """ Called once for each message produced to indicate delivery result.
@@ -48,18 +71,3 @@ def delivery_report(err, msg):
         print(f'Delivery failed for message: {err}', file=sys.stderr)
     else:
         print(f'Message delivered to {msg.topic()} [{msg.partition()}]', file=sys.stderr)
-
-
-def produce_log(log_data):
-    """ Function to produce logs to the 'logs' topic """
-    try:
-        # Produce the log message to the 'logs' topic
-        producer.produce('logs', value=json.dumps(log_data), callback=delivery_report)
-        # Wait up to 1 second for events. Callbacks will be invoked during
-        # this method call if the message is acknowledged.
-        producer.poll(1)
-        print('produce data: ' + json.dumps(log_data), file=sys.stderr)
-        return True
-    except Exception as e:
-        print(f'Failed to produce log: {e}', file=sys.stderr)
-        return False
--- logging_service/run.py
+++ logging_service/run.py
@@ -1,5 +1,3 @@
-from time import sleep
-
 from app import create_app
 
 app = create_app()
@@ -8,6 +6,4 @@ def hello():
     return 'Hello, World! this application runing on 192.168.0.105'
 
 if __name__ == "__main__":
-    print("Starting the application... (sleep 20 seconds)")
-    sleep(20) # wait for kafka to be ready
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000)
